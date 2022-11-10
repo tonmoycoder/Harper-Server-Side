5ad5fee68291dfa2185e91ed5fa00dca48a66fcb
tonmoy <tanverislamtonmoy2929@gmail.com>
2022-11-10 08:53:21 +0600

    limited get products api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 // middle ware
@@ -14,42 +14,56 @@ app.use(express.json());
 
 // mongodb url
 
-
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Password}@cluster0.vtstx9a.mongodb.net/?retryWrites=true&w=majority`;
 
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-async function run(){
-    try{
-        const productCollections = client
-      .db("User_main")
-      .collection("ProductsDetails");
-    const commentCollection = client.db("User_main").collection("userComment");
+async function run() {
+  try {
+    const productCollections = client.db('User_main').collection('ProductsDetails');
+    const commentCollection = client.db('User_main').collection('userComment');
     // get all products api
-    app.get("/products", async (req, res) => {
+    app.get('/products', async (req, res) => {
       const query = {};
       const sort = { time: -1 };
       const curser = productCollections.find(query).sort(sort);
       const result = await curser.toArray();
       res.send(result);
     });
-    }
-    finally{
-
-    }
+    // limited product get api
+    app.get('/products/limit', async (req, res) => {
+      const query = {};
+      const limit = 3;
+      const sort = { time: -1 };
+      const courser = productCollections.find(query).sort(sort).limit(limit);
+      const result = await courser.toArray();
+      res.send(result);
+    });
+    // get single product api
+    app.get('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const user = await productCollections.findOne(query);
+      res.send(user);
+    });
+  } finally {
+  }
 }
 
-run().catch(error => {
-    console.log(error);
-})
+run().catch((error) => {
+  console.log(error);
+});
 
 // Api test
-app.get('/', (req,res) => {
-    res.send('yay api working')
-})
+app.get('/', (req, res) => {
+  res.send('yay api working');
+});
 
 // api port
-app.listen(port,()=>{
-    console.log(`api working on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`api working on port ${port}`);
+});
