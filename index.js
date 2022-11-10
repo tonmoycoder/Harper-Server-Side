d82958313bc841bc8ac42f0b9c9c1a3ddce2b599
tonmoy <tanverislamtonmoy2929@gmail.com>
2022-11-10 07:34:35 +0600

    mongodb added

--- index.js
+++ index.js
@@ -2,11 +2,33 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 // middle ware
 app.use(cors());
 app.use(express.json());
+
+// assignment11
+// 6WAg58tIHTcSSOMl
+
+// mongodb url
+
+
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Password}@cluster0.vtstx9a.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect(err => {
+  const productCollections = client.db("User_main").collection("ProductsDetails");
+  // perform actions on the collection object
+  console.log('mongodb connected');
+  client.close();
+});
+
+
+
 
 // Api test
 app.get('/', (req,res) => {
